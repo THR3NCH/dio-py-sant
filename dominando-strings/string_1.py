37076a99877973be10ad5cb2a99c10e7da0da721
Luan <azambuja.luanz@gmail.com>
2025-07-10 21:16:17 -0300

    alterações strings

---
+++ dominando-strings/string_1.py
@@ -0,0 +1,15 @@
+nome = "Luan"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = "   Olá, Mundo!   "
+print(texto + ".")  # Imprime o texto original
+print(texto.strip() + ".")  # Remove espaços no início e no fim  
+print(texto.lstrip() + ".")# Remove espaços no início
+print(texto.rstrip() + ".")  # Remove espaços no fim
+
+menu = "Python"
+
+print("####" + menu + "####")
